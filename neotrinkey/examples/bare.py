2cfa954c37f6f5b4266a6a2cdb87a316cbbaeb6a
Rob Wells <rob@robjwells.com>
2022-04-30 15:03:26 +0100

    Bare example: flash blue/green instead of no-op

--- neotrinkey/examples/bare.py
+++ neotrinkey/examples/bare.py
@@ -5,13 +5,17 @@ import board
 from neotrinkey import NeoTrinkey, StatusMonitor
 
 
-def no_op(*args, **kwargs):
-    pass
+def flash_blue(trinkey: NeoTrinkey, update=None, pad=None):
+    trinkey.pixels.flash(0x0000FF)
+
+
+def flash_green(trinkey: NeoTrinkey, update=None, pad=None):
+    trinkey.pixels.flash(0x00FF00)
 
 
 trinkey = NeoTrinkey(board)
 
-monitor = StatusMonitor(trinkey, no_op, no_op)
+monitor = StatusMonitor(trinkey, flash_blue, flash_green)
 
 gc.collect()
 print("Free: {:,}kB\tUsed: {:,}kB".format(gc.mem_free(), gc.mem_alloc()))
