d835db3cdf9661fbb5e17b2e3075a7b41f2c9535
Rob Wells <rob@robjwells.com>
2022-04-30 12:10:24 +0100

    Colours: format with black

--- colours.py
+++ colours.py
@@ -1,18 +1,18 @@
-blue = 0x0009f6
+blue = 0x0009F6
 teal = 0x009669
-green = 0x06f900
-purple = 0x4b00b4
-yellow = 0x847b00
-pink = 0x8d0072
-orange = 0xe41b00
-red = 0xf90006
+green = 0x06F900
+purple = 0x4B00B4
+yellow = 0x847B00
+pink = 0x8D0072
+orange = 0xE41B00
+red = 0xF90006
 
-full_red = 0xff0000
-full_green = 0x00ff00
-full_blue = 0x0000ff
+full_red = 0xFF0000
+full_green = 0x00FF00
+full_blue = 0x0000FF
 
 low_red = 0x100000
 low_green = 0x001000
 low_blue = 0x000010
 
-white = 0xffffff
+white = 0xFFFFFF
