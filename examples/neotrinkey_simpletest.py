acaf97b924e572fbfa5142d8543496322a0345e3
Rob Wells <rob@robjwells.com>
2022-05-07 17:25:30 +0100

    Examples: make simpletest more explicit

--- examples/neotrinkey_simpletest.py
+++ examples/neotrinkey_simpletest.py
@@ -5,19 +5,22 @@ import gc
 
 import board
 
-from neotrinkey import NeoTrinkey, StatusMonitor
+from robjwells_neotrinkey import NeoTrinkey, StatusMonitor
+
+BLUE = 0x00_00_FF
+GREEN = 0x00_FF_00
 
 
 def flash_blue(trinkey: NeoTrinkey, update=None, pad=None):
-    trinkey.pixels.flash(0x0000FF)
+    trinkey.pixels.flash(BLUE)
 
 
 def flash_green(trinkey: NeoTrinkey, update=None, pad=None):
-    trinkey.pixels.flash(0x00FF00)
+    trinkey.pixels.flash(GREEN)
 
 
 trinkey = NeoTrinkey(board)
-monitor = StatusMonitor(trinkey, flash_blue, flash_green)
+monitor = StatusMonitor(trinkey, serial_handler=flash_blue, touch_handler=flash_green)
 
 gc.collect()
 print("Free: {:,}kB\tUsed: {:,}kB".format(gc.mem_free(), gc.mem_alloc()))
